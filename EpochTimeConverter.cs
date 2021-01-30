df41b385d7050bd32b0d62bf6736c5caaa125eb5
betsyscott <sscott14@luc.edu>
2021-01-29 19:24:05 -0600

    Add project files.

---
+++ EpochTimeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class EpochTimeConverter
+{
+
+	public DateTime EpochToDateTime(long epochtime)
+	{
+		System.DateTime ConvertedDate = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+		ConvertedDate = ConvertedDate.AddMilliseconds(epochtime).ToLocalTime();
+		return ConvertedDate;
+	}
+
+	public long DateTimeToEpoch(DateTime MyDateTime)
+	{
+		TimeSpan ConvertedEpoch = MyDateTime - new DateTime(1970, 1, 1, 0, 0, 0);
+		return (long)ConvertedEpoch.TotalSeconds;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace DateConverter
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            EpochTimeConverter dtConverter = new EpochTimeConverter();
+            DateTime convertedEpochTime = new DateTime();
+            DateTime DateToBeConverted = new DateTime(2020, 6, 10, 15, 24, 16);
+            long convertedDateTime = new long(); 
+
+            convertedEpochTime = dtConverter.EpochToDateTime(1611795435000);
+            convertedDateTime = dtConverter.DateTimeToEpoch(DateToBeConverted);
+
+
+            Console.WriteLine(convertedEpochTime);
+            Console.WriteLine(convertedDateTime);
+        }
+    }
+}
